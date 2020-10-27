11e7d11a0d196eb079634b16e190f8e5fb5083c9
Dmitry S <dsagal+git@gmail.com>
2020-10-27 15:28:24 -0400

    Scale content to widget width; add LabelCount support; remove single-record support, since impossible to distinguish linking mode

--- printlabel/printlabel.js
+++ printlabel/printlabel.js
@@ -35,8 +35,6 @@ let templates = [{
 let startTemplateId = localStorage.getItem('printlabel-template');
 let startTemplate = templates.find(t => t.id === startTemplateId) || templates[1];
 
-let useListFromRow = false;
-
 let app = undefined;
 let data = {
   status: 'waiting',
@@ -71,46 +69,49 @@ function handleError(err) {
   console.log(data);
 }
 
-function updateRecord(row) {
-  try {
-    if (row && Array.isArray(row.LabelText)) {
-      useListFromRow = true;
-      if (!row.LabelText.length) {
-        throw new Error("No data. Please add some rows");
-      }
-      data.status = '';
-      data.labels = row.LabelText;
-    } else {
-      useListFromRow = false;
-    }
-  } catch (err) {
-    handleError(err);
-  }
-}
-
 function updateRecords(rows) {
-  if (useListFromRow) {
-    return;
-  }
   try {
     data.status = '';
     if (!rows || !rows.length) {
       throw new Error("No data. Please add some rows");
     }
-    if (!rows[0].LabelText) {
+    if (!rows[0].hasOwnProperty('LabelText')) {
       throw new Error('Need a visible column named "LabelText"');
     }
-    data.labels = rows.map(r => r.LabelText);
+    let haveCounts = rows[0].hasOwnProperty('LabelCount');
+    let labels = [];
+    for (let r of rows) {
+      // parseFloat to be generous about the type of LabelCount. Text will be accepted.
+      let count = haveCounts ? parseFloat(r.LabelCount) : 1;
+      for (let i = 0; i < count; i++) {
+        labels.push(r.LabelText);
+      }
+    }
+    data.labels = labels;
+    setTimeout(updateSize, 0);
   } catch (err) {
     handleError(err);
   }
 }
 
+// Page width before any scaling is applied.
+let pageWidth = null;
+
+function updateSize() {
+  let page = document.querySelector('.page-outer');
+  if (!page) { return; }
+  if (!pageWidth) {
+    pageWidth = page.getBoundingClientRect().width;
+  }
+  let content = document.querySelector('#content');
+  content.style.setProperty('--page-scaling', window.innerWidth / pageWidth);
+}
+
 ready(function() {
   // Update the widget anytime the document data changes.
   grist.ready();
-  grist.onRecord(updateRecord);
   grist.onRecords(updateRecords);
+  window.onresize = updateSize;
 
   Vue.config.errorHandler = handleError;
   app = new Vue({
