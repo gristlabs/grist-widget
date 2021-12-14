a057bb5ed668ce1e5e39a7af32b8681770a8a2f1
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2021-12-14 20:17:09 +0100

    Adding onConfig widget

--- inspect/config.js
+++ inspect/config.js
@@ -1,18 +1,21 @@
 // Handler for onOptions event, invoked after we send ready message
 grist.onOptions(options => {
   if (options?.name) {
-    document.getElementById("config_name").value = options.name;
+    document.getElementById("textbox").value = options.name;
   }
 });
 
 // Save button handler
 async function saveConfig() {
-  const name = document.getElementById('config_name').value;
+  const name = document.getElementById('textbox').value;
   try {
+    if (!grist.widgetApi) {
+      throw new Error("WidgetApi is not available");
+    }
     await grist.widgetApi.saveOptions({name});
     closeConfig();
   } catch (err) {
-    alert(String(err));
+    alert(err.message || String(err));
   }
 };
 
@@ -21,6 +24,5 @@ function closeConfig() {
   grist.rpc.callRemoteFunc("closePopup");
 }
 
-// Message Grist that we are ready
+// Message Grist that we are ready.
 grist.ready();
-
