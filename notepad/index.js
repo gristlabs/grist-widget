5ce7a8efab9ca151fdd947127e07eed354e436b6
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2022-04-12 22:48:19 +0200

    New widget - Notepad - rich text editor.

---
+++ notepad/index.js
@@ -0,0 +1,79 @@
+const {Subject} = rxjs;
+const {debounceTime} = rxjs.operators;
+
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+  
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
+// Create quill editor
+const quill = new Quill('#editor', {
+  theme: 'snow',
+  modules: {
+    toolbar: toolbarOptions,
+  },
+});
+
+let column;
+let id;
+let lastContent;
+let lastSave;
+let tableId;
+
+// Subscribe to grist data
+grist.ready({requiredAccess: 'full', columns: [{name: 'Content', type: 'Text'}]});
+grist.onRecord(function (record, mappings) {
+  // If this is a new record, or mapping is diffrent.
+  if (id !== record.id || mappings?.Content !== column) {
+    id = record.id;
+    column = mappings?.Content;
+    const mapped = grist.mapColumnNames(record);
+    if (!mapped) {
+      // Log but don't bother user - maybe we are just testing.
+      console.error('Please map columns');
+    } else if (lastContent !== mapped.Content) {
+      lastContent = mapped.Content;
+      // We will remember last thing sent, to not remove progress.
+      quill.setContents(mapped.Content ? JSON.parse(mapped.Content) : null);
+    }
+  }
+});
+
+// Create a change event.
+const textChanged = new Subject();
+quill.on('text-change', () => textChanged.next(null));
+// Debounce the save event, to not save more often than once every 500 ms.
+const saveEvent = textChanged.pipe(debounceTime(500));
+const table = grist.getTable();
+saveEvent.subscribe(() => {
+  // If we are in a middle of saving, skip this.
+  if (lastSave) { return; }
+  const content = quill.getContents();
+  // If we are mapped.
+  if (column) {
+    // Store content as json.
+    const newContent = JSON.stringify(content);
+    // Don't send what we just received.
+    if (newContent === lastContent) {
+      return;
+    }
+    lastContent = newContent;
+    lastSave = table.update({id, fields: {
+      [column]: lastContent,
+    }}).finally(() => lastSave = null);
+  }
+});
