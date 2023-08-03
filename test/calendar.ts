dfbe568861094a4f08c1963c259a37874e4b7c6e
Jakub Serafin <sirthaven@gmail.com>
2023-08-03 17:03:39 +0200

    unit tests for calendar

---
+++ test/calendar.ts
@@ -0,0 +1,19 @@
+import { assert } from 'mocha-webdriver';
+import {getGrist} from "./getGrist";
+
+describe('calendar', function() {
+    this.timeout(20000);
+    const grist = getGrist();
+
+    it('should show valid event', async function () {
+    });
+    it('should show valid whole-day event', async function () {
+    });
+    it('should not show event with invalid start date', async function () {
+    });
+    it('should update event when table data is changed', async function () {
+    });
+    it('should update table is event is changed', async function () {
+    });
+    it()
+});
