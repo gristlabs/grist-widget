9efdf7a2647f82b199b11566f6169db37ed0543e
Jakub Serafin <sirthaven@gmail.com>
2023-08-08 05:25:53 +0200

    comments

--- calendar/page.js
+++ calendar/page.js
@@ -1,22 +1,6 @@
 // lets assume that it's imported in a html file
 var grist;
 
-document.addEventListener('DOMContentLoaded', ()=> {
-  this.calendarHandler = new CalendarHandler();
-  configureGristSettings();
-
-});
-
-async function calendarViewChanges(radiobutton) {
-  await grist.setOption('calendarViewPerspective', radiobutton.value);
-}
-
-function convertEventToGristTableFormat(event) {
-  const mappedRecord = grist.mapColumnNamesBack(event);
-  delete mappedRecord.id;
-  return { id: event.id, fields: mappedRecord };
-}
-
 class CalendarHandler {
   static _mainColor = getComputedStyle(document.documentElement)
       .getPropertyValue('--main-color');
@@ -63,6 +47,7 @@ class CalendarHandler {
     });
   }
 
+  // navigate to the selected date in the calendar and scroll to the time period of the event
   selectRecord(record) {
     if (this._selectedRecordId) {
       this.calendar.updateEvent(this._selectedRecordId, 'cal1', {backgroundColor: CalendarHandler._mainColor});
@@ -77,28 +62,36 @@ class CalendarHandler {
 
   }
 
+  // change calendar perspective between week, month and day.
   changeView(calendarViewPerspective) {
     this.calendar.changeView(calendarViewPerspective);
   }
 
+  // navigate to the previous time period
   calendarPrevious() {
     this.calendar.prev();
   }
 
+  // navigate to the next time period
   calendarNext() {
     this.calendar.next();
   }
 
+  //navigate to today
   calendarToday() {
     this.calendar.today();
   }
 
+  // update calendar events based on the collection of records from the grist table.
   async updateCalendarEvents(calendarEvents) {
+    // we need to keep track of the ids of the events that are currently in the calendar to compare it
+    // with the new set of events when they come.
     const currentIds = new Set();
+
     for (const record of calendarEvents) {
+      //chek if event already exist in the calendar - update it if so, create new otherwise
       const event = this.calendar.getEvent(record.id, 'cal1');
       const eventData = record;
-
       if (!event) {
         this.calendar.createEvents([eventData]);
       } else {
@@ -106,6 +99,7 @@ class CalendarHandler {
       }
       currentIds.add(record.id);
     }
+    // if some events are not in the new set of events, we need to remove them from the calendar
     if(this.previousIds) {
       for (const id of this.previousIds) {
         if (!currentIds.has(id)) {
@@ -117,6 +111,22 @@ class CalendarHandler {
   }
 }
 
+// when document is ready, register calendar and subscribe to grist events
+document.addEventListener('DOMContentLoaded', ()=> {
+  this.calendarHandler = new CalendarHandler();
+  configureGristSettings();
+});
+
+//to update the table, grist require other format that it is returning in onRecords event (it's flat there),
+// so it need to be converted
+function convertEventToGristTableFormat(event) {
+  const mappedRecord = grist.mapColumnNamesBack(event);
+  // we cannot save record is some unexpected columns are defined in fields, so we need to remove them
+  delete mappedRecord.id;
+  return { id: event.id, fields: mappedRecord };
+}
+
+// Data for column mapping fileds in Widget GUI
 function getGristOptions() {
   return [
     {
@@ -153,34 +163,52 @@ function getGristOptions() {
   ];
 }
 
+// let subscribe all the events that we need
 async function configureGristSettings() {
-  const columnsMappingOptions = getGristOptions();
-
+  // table selection should change when other event is selected
   grist.allowSelectBy();
-  grist.onRecords(updateCalendar2);
+  // CRUD operations on records in table
+  grist.onRecords(updateCalendar);
+  // When cursor (selected record) change in the table
   grist.onRecord(gristSelectedRecordChanged);
+  // When options changed in the widget configuration (reaction to perspective change)
   grist.onOptions(onGristSettingsChanged);
+
+  // bind columns mapping options to the GUI
+  const columnsMappingOptions = getGristOptions();
   grist.ready({ requiredAccess: 'read table', columns: columnsMappingOptions });
 }
 
+// when user select record in the table, we want to select it on the calendar
   function gristSelectedRecordChanged(record, mappings) {
     const mappedRecord = grist.mapColumnNames(record, mappings);
     if (mappedRecord && this.calendarHandler) {
       this.calendarHandler.selectRecord(mappedRecord);
     }
-  }
+}
 
+// when user change the perspective in the GUI, we want to save it as grist option
+// - rest of logic is in reaction to grist option changed
+async function calendarViewChanges(radiobutton) {
+  await grist.setOption('calendarViewPerspective', radiobutton.value);
+}
+
+
+// when user change a perspective of calendar we want this to be persisted in grist options between sessions.
+// This is the place where we can react to this change and update calendar view, or when new session is started
+// (so we are loading previous settings)
 let onGristSettingsChanged = function(options) {
   let option = options?.calendarViewPerspective??'week';
     this.calendarHandler.changeView(option);
     selectRadioButton(option);
 };
 
+// when user move or resize event on the calendar, we want to update the record in the table
 const onCalendarEventBeingUpdated = async (info) => {
     if (info.changes?.start || info.changes?.end) {
       const record =  await grist.fetchSelectedRecord(info.event.id);
       if (record) {
-
+        // get all the record data from the event, and update only start and end date
         const gristEvent = buildGristFlatFormatFromEventObject(info.event)
         if(info.changes.start) gristEvent.startDate = roundEpochDateToSeconds(info.changes.start.valueOf());
         if(info.changes.end) gristEvent.endDate = roundEpochDateToSeconds(info.changes.end.valueOf());
@@ -189,6 +217,7 @@ const onCalendarEventBeingUpdated = async (info) => {
     }
 };
 
+// saving events to the table or updating existing one - basing on if ID is present or not in the send event
 async function upsertGristRecord(gristEvent){
     const eventInValidFormat = convertEventToGristTableFormat(gristEvent);
     const table = await grist.getTable();
@@ -198,9 +227,14 @@ async function upsertGristRecord(gristEvent){
         await table.create(eventInValidFormat);
     }
 }
+
+// grist expect date in seconds, but calendar is returning it in miliseconds, so we need to convert it
 function roundEpochDateToSeconds(date) {
   return date/1000;
 }
+
+// conversion between calendar event object and grist flat format (so the one that is returned in onRecords event
+// and can be mapped by grist.mapColumnNamesBack)
 function buildGristFlatFormatFromEventObject(TUIEvent) {
   const gristEvent = {
     startDate: roundEpochDateToSeconds(TUIEvent.start?.valueOf()),
@@ -212,11 +246,13 @@ function buildGristFlatFormatFromEventObject(TUIEvent) {
   return gristEvent;
 }
 
+// when user select new date range on the calendar, we want to create new record in the table
 const onNewDateBeingSelectedOnCalendar = async (info) => {
   const gristEvent = buildGristFlatFormatFromEventObject(info);
   upsertGristRecord(gristEvent);
 }
 
+//helper function to select radio button in the GUI
 function selectRadioButton(value) {
   for (const element of document.getElementsByName('calendar-options')) {
     if (element.value === value) {
@@ -225,7 +261,7 @@ function selectRadioButton(value) {
   }
 }
 
-
+// helper function to build calendar event object from grist flat record
 function buildCalendarEventObject(record) {
   return {
     id: record.id,
@@ -239,7 +275,8 @@ function buildCalendarEventObject(record) {
   };
 }
 
-async function updateCalendar2(records, mappings) {
+// when some CRUD operation is performed on the table, we want to update calendar
+async function updateCalendar(records, mappings) {
   const mappedRecords = grist.mapColumnNames(records, mappings);
   // if any records was successfully mapped, create or update them in the calendar
   if (mappedRecords) {
