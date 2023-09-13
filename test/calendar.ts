6ae5178e31184253c0960dc56e9d8388dd7f6384
Jakub Serafin <sirthaven@gmail.com>
2023-09-13 11:36:09 +0200

    trying to force closing doc in calendar tests to force popup to appear there.

--- test/calendar.ts
+++ test/calendar.ts
@@ -51,7 +51,7 @@ describe('calendar', function () {
     this.afterAll(async function () {
         //In case "Changes you made may not be saved" dialog is shown, we need to close it.
         try{
-            grist.closeDoc();
+            await grist.closeDoc();
             await driver.switchTo().alert().accept();
         }
         catch (e) {
