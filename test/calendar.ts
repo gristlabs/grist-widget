a9f2d920062ace406803e43b9d57ded56a708e7d
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2023-10-11 08:52:19 +0200

    Reverting test fixes

--- test/calendar.ts
+++ test/calendar.ts
@@ -189,7 +189,6 @@ describe('calendar', function () {
 
   it("should respect non full access", async function () {
     await grist.setCustomWidgetAccess('none');
-    await grist.rejectAccess();
     await grist.waitForServer();
     await grist.waitForFrame();
 
@@ -201,7 +200,6 @@ describe('calendar', function () {
 
     // Now with read access.
     await grist.setCustomWidgetAccess('read table');
-    await grist.rejectAccess();
     await grist.waitForServer();
     await grist.waitForFrame();
 
@@ -247,7 +245,7 @@ describe('calendar', function () {
     });
 
     // Select 2 row in grid view.
-    await selectRow(2);
+    await clickRow(2);
 
     assert.equal(await selectedRow(), 2);
 
@@ -255,7 +253,7 @@ describe('calendar', function () {
     assert.isTrue(await getCalendarEvent(3).then(c => c.selected));
 
     // Click 4th row
-    await selectRow(3);
+    await clickRow(3);
     assert.equal(await selectedRow(), 3);
     assert.isTrue(await getCalendarEvent(4).then(c => c.selected));
 
@@ -332,7 +330,7 @@ describe('calendar', function () {
     });
   }
 
-  async function selectRow(rowIndex: number) {
+  async function clickRow(rowIndex: number) {
     await driver.findContentWait('.gridview_data_row_num', String(rowIndex), 200).click();
   }
 
