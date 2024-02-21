f3fbf6c8aa7846e4eaec1970a885d456c28e550a
jperon <cataclop@hotmail.com>
2024-02-21 11:50:23 +0100

    Allow several action buttons (#124)
    If, instead of a dictionary, a list of dictionaries is defined, several buttons will be displayed.

--- actionbutton/actionbutton.js
+++ actionbutton/actionbutton.js
@@ -11,11 +11,12 @@ let app = undefined;
 let data = {
   status: 'waiting',
   result: null,
-  input: {
+  inputs: [{
     description: null,
     button: null,
     actions: null,
-  }
+  }],
+  desc: null
 }
 
 function handleError(err) {
@@ -23,10 +24,10 @@ function handleError(err) {
   data.status = String(err).replace(/^Error: /, '');
 }
 
-async function applyActions() {
+async function applyActions(actions) {
   data.results = "Working...";
   try {
-    await grist.docApi.applyUserActions(data.input.actions);
+    await grist.docApi.applyUserActions(actions);
     data.message = 'Done';
   } catch (e) {
     data.message = `Please grant full access for writing. (${e})`;
@@ -42,15 +43,22 @@ function onRecord(row, mappings) {
     if (!row.hasOwnProperty(column)) {
       throw new Error(`Need a visible column named "${column}". You can map a custom column in the Creator Panel.`);
     }
-    const keys = ['button', 'description', 'actions'];
-    if (!row[column] || keys.some(k => !row[column][k])) {
-      const allKeys = keys.map(k => JSON.stringify(k)).join(", ");
-      const missing = keys.filter(k => !row[column]?.[k]).map(k => JSON.stringify(k)).join(", ");
-      const gristName = mappings?.[column] || column;
-      throw new Error(`"${gristName}" cells should contain an object with keys ${allKeys}. ` +
-        `Missing keys: ${missing}`);
+    let btns = row[column]
+    // If only one action button is defined, put it within an Array
+    if (!Array.isArray(btns)) {
+      btns = [ btns ]
     }
-    data.input = row[column];
+    const keys = ['button', 'description', 'actions'];
+    for (btn of btns) {
+      if (!btn || keys.some(k => !btn[k])) {
+        const allKeys = keys.map(k => JSON.stringify(k)).join(", ");
+        const missing = keys.filter(k => !btn?.[k]).map(k => JSON.stringify(k)).join(", ");
+        const gristName = mappings?.[column] || column;
+        throw new Error(`"${gristName}" cells should contain an object with keys ${allKeys}. ` +
+          `Missing keys: ${missing}`);
+      }
+    }
+    data.inputs = btns;
   } catch (err) {
     handleError(err);
   }
