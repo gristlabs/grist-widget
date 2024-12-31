4287adc03905151b8fe6fc7235361a9079531dab
sgfroerer <97491750+sgfroerer@users.noreply.github.com>
2024-12-30 21:28:45 -0800

    Update main.js

--- ArcGIS-API/main.js
+++ ArcGIS-API/main.js
@@ -1,22 +1,42 @@
 require([
   "esri/Map",
   "esri/views/MapView",
-  "esri/layers/TileLayer"
-], function (Map, MapView, TileLayer) {
-
-  var layer = new TileLayer({
-    url: "https://imagery.oregonexplorer.info/arcgis/rest/services/NAIP_2009/NAIP_2009_WM/ImageServer",
-  });
-
+  "esri/layers/FeatureLayer"
+], function (Map, MapView, FeatureLayer) {
+  // Create the map with a hybrid basemap
   var map = new Map({
-    basemap: "gray",
-    layers: [layer]
+    basemap: "hybrid"
   });
 
+  // Create the view centered on Oregon
   var view = new MapView({
     container: "myMap",
     map: map,
-    center: [-123, 44],
-    zoom: 13
+    center: [-123.018723, 44.925675], // Longitude, Latitude
+    zoom: 12
   });
+
+  // Define the four layers from the Oregon Parcel Viewer
+  var taxCodeLayer = new FeatureLayer({
+    url: "https://services8.arcgis.com/8PAo5HGmvRMlF2eU/arcgis/rest/services/or_cadastral_wm/FeatureServer/2",
+    title: "Tax Code"
+  });
+
+  var mapIndexLayer = new FeatureLayer({
+    url: "https://services8.arcgis.com/8PAo5HGmvRMlF2eU/arcgis/rest/services/or_cadastral_wm/FeatureServer/3",
+    title: "Map Index"
+  });
+
+  var taxLotLayer = new FeatureLayer({
+    url: "https://services8.arcgis.com/8PAo5HGmvRMlF2eU/arcgis/rest/services/or_cadastral_wm/FeatureServer/4",
+    title: "Tax Lot"
+  });
+
+  var realPropertyTableLayer = new FeatureLayer({
+    url: "https://services8.arcgis.com/8PAo5HGmvRMlF2eU/arcgis/rest/services/or_cadastral_wm/FeatureServer/6",
+    title: "Real Property Table"
+  });
+
+  // Add the layers to the map
+  map.addMany([taxCodeLayer, mapIndexLayer, taxLotLayer, realPropertyTableLayer]);
 });
