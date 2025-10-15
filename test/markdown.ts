28353e544b690d6541ec503f1fcda17318d0e993
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2025-10-15 16:08:53 +0200

    Updating tests

--- test/markdown.ts
+++ test/markdown.ts
@@ -2,48 +2,49 @@ import { Key, assert } from 'mocha-webdriver';
 import { getGrist } from 'test/getGrist';
 
 describe('markdown', function() {
-  this.timeout(20000);
-  const grist = getGrist();
+  this.timeout(200000);
+  const gu = getGrist();
+  gu.bigScreen();
 
   it('renders text as markdown', async function() {
-    const docId = await grist.upload('test/fixtures/docs/SchoolsSample.grist');
-    await grist.openDoc(docId);
-    await grist.waitToPass(async () => {
-      await grist.dismissBehavioralPrompts();
+    const docId = await gu.upload('test/fixtures/docs/SchoolsSample.grist');
+    await gu.openDoc(docId);
+    await gu.waitToPass(async () => {
+      await gu.dismissBehavioralPrompts();
     });
-    await grist.toggleSidePanel('right', 'open');
-    await grist.addNewSection(/Custom/, /School/, {dismissTips: true});
-    await grist.clickWidgetGallery();
-    await grist.selectCustomWidget(/Markdown/);
-    await grist.setCustomWidgetAccess('full');
-    await grist.setCustomWidgetMapping('Content', /School Head/);
-    await grist.waitToPass(async () => {
-      const paragraph = await grist.getCustomWidgetBody('.editor-preview > p');
+    await gu.toggleSidePanel('right', 'open');
+    await gu.addNewSection(/Custom/, /School/, {dismissTips: true});
+    await gu.clickWidgetGallery();
+    await gu.selectCustomWidget(/Markdown/);
+    await gu.driver.findWait('.test-custom-widget-not-mapped', 6000);
+    await gu.setCustomWidgetAccess('full');
+    await gu.setCustomWidgetMapping('Content', /School Head/);
+    await gu.waitToPass(async () => {
+      const paragraph = await gu.getCustomWidgetBody('.editor-preview > p');
       assert.equal(paragraph, 'SUPERINTENDENT - DR. MARGUERITE VANDEN WYNGAARD');
     });
-    await grist.inCustomWidget(async () => {
-      await grist.driver.find('button.edit').click();
-      await grist.driver.sendKeys('# ');
-      await grist.driver.find('button.save').click();
+    await gu.inCustomWidget(async () => {
+      await gu.driver.find('button.edit').click();
+      await gu.driver.sendKeys('# ');
+      await gu.driver.find('button.save').click();
     });
-    await grist.waitToPass(async () => {
-      const heading = await grist.getCustomWidgetBody('.editor-preview > h1');
+    await gu.waitToPass(async () => {
+      const heading = await gu.getCustomWidgetBody('.editor-preview > h1');
       assert.equal(heading, 'SUPERINTENDENT - DR. MARGUERITE VANDEN WYNGAARD');
     });
   });
 
   it('saves on blur', async function() {
-    await grist.inCustomWidget(async () => {
-      await grist.driver.find('button.edit').click();
-      await grist.driver.sendKeys(Key.END, Key.ENTER, Key.ENTER,
+    await gu.inCustomWidget(async () => {
+      await gu.driver.find('button.edit').click();
+      await gu.driver.sendKeys(Key.END, Key.ENTER, Key.ENTER,
         ' - Phone Number: (123) 456-7890');
     });
-    await grist.driver.findWait('.test-dm-account', 2000).click();
-    await grist.dismissBehavioralPrompts();
-    await grist.waitToPass(async () => {
-      const heading = await grist.getCustomWidgetBody('.editor-preview > h1');
+    await gu.driver.findWait('#grist-tools-heading', 2000).click();
+    await gu.waitToPass(async () => {
+      const heading = await gu.getCustomWidgetBody('.editor-preview > h1');
       assert.equal(heading, 'SUPERINTENDENT - DR. MARGUERITE VANDEN WYNGAARD');
-      const listItem = await grist.getCustomWidgetBody('.editor-preview > ul > li');
+      const listItem = await gu.getCustomWidgetBody('.editor-preview > ul > li');
       assert.equal(listItem, 'Phone Number: (123) 456-7890');
     });
   });
