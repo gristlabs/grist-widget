c49a57ed5fe04dc64b68f2d05e9166da8de62bf2
Dmitry S <dsagal+git@gmail.com>
2025-11-16 02:32:26 -0500

    Add back in live-reload functionality

--- buildtools/dev-server.js
+++ buildtools/dev-server.js
@@ -1,11 +1,21 @@
 const http = require('node:http');
 const sirv = require('sirv');
+const livereload = require('livereload');
+const connectLR = require('connect-livereload');
 const rewrite = require('./rewriteUrl');
 
 const WIDGETS_PORT = process.env.PORT || 8585;
 
+livereload.createServer().watch('.');
+const lrMiddleware = connectLR();
 const serve = sirv('.', { dev: true });
 
 http.createServer((req, res) => {
-  rewrite(req, res, () => serve(req, res));
-}).listen(WIDGETS_PORT);
+  lrMiddleware(req, res, () => {
+    rewrite(req, res, () => {
+      serve(req, res);
+    });
+  });
+}).listen(WIDGETS_PORT, () => {
+  console.log(`Server running on: http://localhost:${WIDGETS_PORT}`);
+});
